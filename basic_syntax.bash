b983fc671b86b125c9e576f69f61d78ec6f93ce4
Temis <temis.barreto@newryglobal.com>
2024-06-13 15:52:55 +0200

    feat: bash basics syntax

---
+++ basic_syntax.bash
@@ -0,0 +1,15 @@
+# Variables
+# Name and age
+name="Temis"
+age=22
+# Conditions 
+if [ "$age" -ge 18 ]; then
+    echo "Hi"
+else
+    echo "Hi2"
+fi
+
+# For loop
+for i in 1 2 3; do
+	echo "Number $i"
+done

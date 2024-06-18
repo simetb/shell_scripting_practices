a713eab093820c39bf4c65d690c4e99bac46707f
Temis <temispita9000@gmail.com>
2024-06-18 23:38:40 +0200

    feat: exc_12

--- exercises/exc_12.bash
+++ exercises/exc_12.bash
@@ -1,14 +1,7 @@
 #!/bin/bash
-read -p "Fractal iterations: " iter
-
-# Dimension of the matrix
-rows=100
-cols=65
-
 initialize_matrix() {
 	for ((i=0; i<rows; i++)); do
-    		for ((j=0; j<cols; j++)); do
-      
+		for ((j=0; j<cols; j++)); do
 			matrix[$((i * cols + j))]=0
 		done
 	done
@@ -25,12 +18,31 @@ display_matrix() {
 
 do_fractals(){
 	# Do the stem
-	echo "Im doing a stem"
+	# echo "Im doing a stem $1"
+	local input=$1
+	if [ $1 -eq 16 ]; then
+		echo "First iteration $base"
+	fi
+
+	base=$(( input / 2 ))
 
 	# Do the branches
-	echo "Im doing the branches"
+	# echo "Im doing the branches"
 }
+
+# Dimension of the matrix
+rows=100
+cols=65
+base=16
+
+read -p "Fractal iterations: " iter
+
 
 initialize_matrix
 
-display_matrix
+#display_matrix
+
+for((k=0; k<iter; k++)); do
+	do_fractals "$base"
+	echo "Base after iterations $((k+1)): $base"
+done	
